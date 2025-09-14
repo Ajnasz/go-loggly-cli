f61470e35bf9d2d9624e6065b81fb1d34f8b51e8
Lajos Koszti <ajnasz@ajnasz.hu>
2025-09-14 16:10:00 +0200

    refactor(buffer): simplify locking and sending logic
    Replaced RWMutex with Mutex and refactored send method to use a loop
    for sequential sending. This improves code clarity and ensures proper
    locking and unlocking.

--- orderedbuffer/orderedbuffer.go
+++ orderedbuffer/orderedbuffer.go
@@ -6,39 +6,40 @@ import (
 
 type OrderedBuffer[T any] struct {
 	responses   map[int]T
-	mu          *sync.RWMutex
+	mu          sync.Mutex
 	lastSentIdx int
 	ch          chan T
 }
 
 func NewOrderedBuffer[T any](ch chan T) *OrderedBuffer[T] {
-	var mu sync.RWMutex
 	return &OrderedBuffer[T]{
 		responses:   make(map[int]T),
 		ch:          ch,
 		lastSentIdx: -1,
-		mu:          &mu,
 	}
 }
 
 func (s *OrderedBuffer[T]) send() {
-	s.mu.Lock()
-	newIdx := s.lastSentIdx + 1
-	if resp, ok := s.responses[newIdx]; ok {
-		s.ch <- resp
+	for {
+		s.mu.Lock()
+		newIdx := s.lastSentIdx + 1
+		resp, ok := s.responses[newIdx]
+		if !ok {
+			s.mu.Unlock()
+			return
+		}
 		s.lastSentIdx = newIdx
 		delete(s.responses, newIdx)
 		s.mu.Unlock()
-		s.send()
-		return
-	} else {
+
+		s.ch <- resp
 	}
-	s.mu.Unlock()
 }
 
 func (s *OrderedBuffer[T]) Store(i int, r T) {
 	s.mu.Lock()
 	s.responses[i] = r
 	s.mu.Unlock()
+
 	s.send()
 }
